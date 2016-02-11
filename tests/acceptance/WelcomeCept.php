d8bba4c7ad16e6be0ebd7cceabd27bc67af2744b
Daris Strickland <daris.strickland@gmail.com>
2016-02-11 09:46:31 -0500

    added phantomjs

--- tests/acceptance/WelcomeCept.php
+++ tests/acceptance/WelcomeCept.php
@@ -8,3 +8,17 @@ $I->see('Just another WordPress site');
 $I->login('tester', 'test');
 
 $I->see('Howdy, tester');
+
+$I->amOnPage('/wp/wp-admin/post.php?post=4&action=edit');
+
+$I->see('+ Add Field');
+
+$I->click('+ Add Field');
+
+$I->fillField('.field-label', 'acf address test field');
+
+$I->selectOption('form select.field-type', 'address');
+
+$I->wait(3);
+
+$I->seeElement('.acf-address-1-row');
